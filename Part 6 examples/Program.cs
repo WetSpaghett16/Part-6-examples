0a6dd3737439e1b8667125f4421cf89177ce6fbf
David Thomas <ThomDavi727@GoTVDSB.ca>
2020-10-16 12:56:34 -0400

    Add project files.

---
+++ Part 6 examples/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Part_6_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int selection;
+            do
+            {
+                Console.WriteLine("Who is the better captain?");
+                Console.WriteLine("1 - Captain Picard");
+                Console.WriteLine("2 - Captain Kirk");
+                selection = Convert.ToInt32(Console.ReadLine());
+            } while (selection != 1);
+            Console.WriteLine("Correct");
+        }
+        }
+    }
+
